de053b30a468bc12efb6dcdb2b89350154c2b779
VISHAL <VISHAL@DESKTOP-PC1SNN3>
2022-10-08 12:59:11 +0530

    initial import

---
+++ GitDemo/src/com/vny/demo/Bank.java
@@ -0,0 +1,10 @@
+package com.vny.demo;
+
+public class Bank {
+
+	public static void main(String[] args) {
+		System.out.println("hello GitHub");
+
+	}
+
+}
---
+++ GitDemo/src/com/vny/test/TestGit.java
@@ -0,0 +1,9 @@
+package com.vny.test;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		System.out.println("hello vishal..");
+	}
+
+}
